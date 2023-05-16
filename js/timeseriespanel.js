eaa7e02420269db5ef7c73f041ac88da85235dee
myles <mrmylesmc@gmail.com>
2023-05-15 23:14:29 -0500

    timeseriespanel.js dev

--- js/timeseriespanel.js
+++ js/timeseriespanel.js
@@ -4,12 +4,46 @@ function toggleTimeseriesPanel() {
         // console.log('hide timeseries panel');
         document.getElementById("timeseriespanel").style.visibility = "hidden"
     } else {
+        var ts_panel = document.getElementById("timeseriespanel")
         // turn on panel
-        document.getElementById("timeseriespanel").style.visibility = "visible"
-        // get TS list
-        
+       ts_panel.style.visibility = "visible"
+        // get timeseries json data
+        fetch(`../data/westPark/timeseries/timeseries.json`)
+        .then(response => response.json())
+        .then(data => {
+            // console.log(data)
+            // console.log(Object.keys(data));
+            // for each event, create plotly trace
+            var tracedata = [];
+            innerHTMLtext = ts_panel.innerHTML
+            Object.keys(data).forEach(key => {
+                // console.log(key, data[key]);
+                console.log(key);
+                console.log(ts_panel);
+                innerHTMLtext +=
+                    `<dt class="row" style="background:#AB2706;display: inline-block;width:16px;height:16px;"></dt>` +
+                    `<dd class="row" style="display: inline-block;font-size:16px;margin-bottom:0;">${key}</dd><br></br>`
+
+            ts_panel.innerHTML = innerHTMLtext
+
+
+                // var values = data[key][name].map(d => d.values);
+                // var times = data[key][name].map(d => d.datetime);
+                // // console.log(times);
+                // var trace =
+                // {
+                //     x: times,
+                //     y: values,
+                //     yaxis: 'WSE (ft)',
+                //     type: 'scatter',
+                //     name: key
+                // }
+                // ;
+                // tracedata.push(trace)
+            });   
+        });
     }
-  }
+}
 
 
 
